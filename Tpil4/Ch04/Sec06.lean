e88de25380487949431787106920a6c6336515be
thesnakefromthelemma <rafayaashary01@gmail.com>
2026-01-22 11:30:11 -0700

    Finished non-definition exercises in Ch04

--- Tpil4/Ch04/Sec06.lean
+++ Tpil4/Ch04/Sec06.lean
@@ -1,7 +1,9 @@
 /- Constructive -/
 namespace ex01
 
-def fp_style : {α : Sort u} → {r : Prop} → (∃ _ : α, r) → r
+def fp_style :
+  {α : Sort u} → {r : Prop} →
+  (∃ _ : α, r) → r
 :=
   fun (Exists.intro _ hr) => hr
 
@@ -11,8 +13,11 @@ end ex01
 /- Constructive -/
 namespace ex02
 
-def fp_style : {α : Sort u} → {r : Prop} → (a : α) → r → (∃ _ : α, r) :=
-Exists.intro
+def fp_style :
+  {α : Sort u} → {r : Prop} →
+  (_ : α) → r → (∃ _ : α, r)
+:=
+  Exists.intro
 
 end ex02
 
@@ -20,12 +25,13 @@ end ex02
 /- Constructive -/
 namespace ex03
 
-def fp_style : {α : Sort u} → {p : α → Prop} → {r : Prop} → (∃ x, p x ∧ r) ↔ (∃ x, p x) ∧ r :=
+def fp_style :
+  {α : Sort u} → {p : α → Prop} → {r : Prop} →
+  (∃ x, p x ∧ r) ↔ (∃ x, p x) ∧ r
+:=
   Iff.intro
-    (fun (Exists.intro x (And.intro hpx hr)) =>
-      And.intro (Exists.intro x hpx) (hr))
-    (fun (And.intro (Exists.intro x hpx) hr) =>
-      Exists.intro x (And.intro hpx hr))
+    (fun (Exists.intro x (And.intro hpx hr)) => And.intro (Exists.intro x hpx) (hr))
+    (fun (And.intro (Exists.intro x hpx) hr) => Exists.intro x (And.intro hpx hr))
 
 end ex03
 
@@ -33,7 +39,10 @@ end ex03
 /- Constructive -/
 namespace ex04
 
-def fp_style : {α : Sort u} → {p q : α → Prop} → (∃ x, p x ∨ q x) ↔ (∃ x, p x) ∨ (∃ x, q x) :=
+def fp_style :
+  {α : Sort u} → {p q : α → Prop} →
+  (∃ x, p x ∨ q x) ↔ (∃ x, p x) ∨ (∃ x, q x)
+:=
   Iff.intro
     (fun
     | Exists.intro x (Or.inl hpx) => Or.inl (Exists.intro x hpx)
@@ -48,8 +57,14 @@ end ex04
 /- Classical -/
 namespace ex05
 
-def fp_style : {α : Sort u} → {p : α → Prop} → (∀ x, p x) ↔ ¬(∃ x, ¬ p x) :=
-  sorry
+def fp_style :
+  {α : Sort u} → {p : α → Prop} →
+  (∀ x, p x) ↔ ¬(∃ x, ¬ p x)
+:=
+  Iff.intro
+    (fun hap (Exists.intro x hnpx) => hnpx (hap x))
+    (fun hnexnpx x => Classical.byContradiction
+      (fun hnpx => hnexnpx (Exists.intro x hnpx)))
 
 end ex05
 
@@ -57,8 +72,14 @@ end ex05
 /- Classical -/
 namespace ex06
 
-def fp_style : {α : Sort u} → {p : α → Prop} → (∃ x, p x) ↔ ¬ (∀ x, ¬ p x) :=
-  sorry
+def fp_style :
+  {α : Sort u} → {p : α → Prop} →
+  (∃ x, p x) ↔ ¬ (∀ x, ¬ p x)
+:=
+  Iff.intro
+    (fun (Exists.intro x hpx) haxnpx => (haxnpx x) hpx)
+    (fun hnaxnpx => Classical.byContradiction
+      (fun hnep => hnaxnpx (fun x hpx => hnep (Exists.intro x hpx))))
 
 end ex06
 
@@ -69,8 +90,15 @@ end ex06
 /- Classical -/
 namespace ex08
 
-def fp_style : {α : Sort u} → {p : α → Prop} → (¬ ∀ x, p x) ↔ (∃ x, ¬ p x) :=
-  sorry
+def fp_style :
+  {α : Sort u} → {p : α → Prop} →
+  (¬ ∀ x, p x) ↔ (∃ x, ¬ p x)
+:=
+  Iff.intro
+    (fun hnap => Classical.byContradiction
+      (fun hnexnpx => hnap (fun x => Classical.byContradiction
+        (fun hnpx => hnexnpx (Exists.intro x hnpx)))))
+    (fun (Exists.intro x hnpx) hap => hnpx (hap x))
 
 end ex08
 
@@ -78,7 +106,10 @@ end ex08
 /- Constructive -/
 namespace ex09
 
-def fp_style : {α : Sort u} → {p : α → Prop} → {r : Prop} → (∀ x, p x → r) ↔ (∃ x, p x) → r :=
+def fp_style :
+  {α : Sort u} → {p : α → Prop} → {r : Prop} →
+  (∀ x, p x → r) ↔ (∃ x, p x) → r
+:=
   Iff.intro
     (fun haxipxr (Exists.intro x hpx) => haxipxr x hpx)
     (fun hiepr x hpx => hiepr (Exists.intro x hpx))
@@ -89,17 +120,31 @@ end ex09
 /- Constructive -/
 namespace ex07
 
-def fp_style : {α : Sort u} → {p : α → Prop} → (¬ ∃ x, p x) ↔ (∀ x, ¬ p x) :=
+def fp_style :
+  {α : Sort u} → {p : α → Prop} →
+  (¬ ∃ x, p x) ↔ (∀ x, ¬ p x)
+:=
   (fun (Iff.intro hl hr) => Iff.intro hr hl) ex09.fp_style
 
 end ex07
 
 
-/- Classical -/
+/- Classical (is there some slicker approach??) -/
 namespace ex10
 
-def fp_style : {α : Sort u} → {p : α → Prop} → {r : Prop} → (a : α) → (∃ x, p x → r) ↔ (∀ x, p x) → r :=
-  sorry
+def fp_style :
+  {α : Sort u} → {p : α → Prop} → {r : Prop} →
+  (_ : α) → (∃ x, p x → r) ↔ (∀ x, p x) → r
+:=
+  fun a =>
+    Iff.intro
+      (fun (Exists.intro x hipxr) hap => hipxr (hap x))
+      (fun hiapr => Classical.byCases
+        (fun hap => Exists.intro a (fun _ => hiapr hap))
+        (fun hnap => Classical.byContradiction
+          fun hnexipxr => hnap (fun x => Classical.byContradiction
+            (fun hnpx => hnexipxr (Exists.intro x (fun hpx => nomatch hnpx hpx))))))
+
 
 end ex10
 
@@ -107,13 +152,16 @@ end ex10
 /- Classical -/
 namespace ex11
 
-def fp_style : {α : Sort u} → {p : α → Prop} → {r : Prop} → (a : α) → (∃ x, r → p x) ↔ (r → ∃ x, p x) :=
+def fp_style :
+  {α : Sort u} → {p : α → Prop} → {r : Prop} →
+  (_ : α) → (∃ x, r → p x) ↔ (r → ∃ x, p x)
+:=
   fun a =>
     Iff.intro
       (fun (Exists.intro x hirpx) hr => Exists.intro x (hirpx hr))
       (fun hirep => Classical.byCases
         (fun hr => match hirep hr with
-        | Exists.intro x hpx => Exists.intro x (fun _ => hpx))
+          | Exists.intro x hpx => Exists.intro x (fun _ => hpx))
         (fun hnr => Exists.intro a (fun hr => nomatch hnr hr)))
 
 end ex11
@@ -122,7 +170,10 @@ end ex11
 /- Constructive -/
 namespace ex12
 
-def fp_style : {α : Sort u} → {p q : α → Prop} → (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) :=
+def fp_style :
+  {α : Sort u} → {p q : α → Prop} →
+  (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x)
+:=
   Iff.intro
     (fun haxapxqx => And.intro
       (fun x => And.left (haxapxqx x))
@@ -135,7 +186,10 @@ end ex12
 /- Constructive -/
 namespace ex13
 
-def fp_style : {α : Sort u} → {p q : α → Prop} → (∀ x, p x → q x) → (∀ x, p x) → (∀ x, q x) :=
+def fp_style :
+  {α : Sort u} → {p q : α → Prop} →
+  (∀ x, p x → q x) → (∀ x, p x) → (∀ x, q x)
+:=
   fun haxipxqx hap x => (haxipxqx x) (hap x)
 
 end ex13
@@ -144,7 +198,10 @@ end ex13
 /- Constructive -/
 namespace ex14
 
-def fp_style : {α : Sort u} → {p q : α → Prop} → (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x :=
+def fp_style :
+  {α : Sort u} → {p q : α → Prop} →
+  (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x
+:=
   fun
   | (Or.inl hap), x => Or.inl (hap x)
   | (Or.inr haq), x => Or.inr (haq x)
@@ -155,7 +212,9 @@ end ex14
 /- Constructive -/
 namespace ex15
 
-def fp_style : {α : Sort u} → {r : Prop} → α → ((∀ x : α, r) ↔ r) :=
+def fp_style :
+  {α : Sort u} → {r : Prop} →
+  α → ((∀ _ : α, r) ↔ r) :=
   fun a =>
     Iff.intro
       (fun har => har a)
@@ -167,8 +226,19 @@ end ex15
 /- Classical -/
 namespace ex16
 
-def fp_style : {α : Sort u} → {p : α → Prop} → {r : Prop} → (∀ x, p x ∨ r) ↔ (∀ x, p x) ∨ r :=
-  sorry
+def fp_style :
+  {α : Sort u} → {p : α → Prop} → {r : Prop} →
+  (∀ x, p x ∨ r) ↔ (∀ x, p x) ∨ r
+:=
+  Iff.intro
+    (Classical.byCases
+      (fun hr _ => Or.inr hr)
+      (fun hnr haxopxr => Or.inl (fun x => match haxopxr x with
+        | Or.inl hpx => hpx
+        | Or.inr hr => nomatch hnr hr)))
+    (fun
+    | Or.inl hap, x => Or.inl (hap x)
+    | Or.inr hr, _ => Or.inr hr)
 
 end ex16
 
@@ -176,7 +246,10 @@ end ex16
 /- Classical -/
 namespace ex17
 
-def fp_style : {α : Sort u} → {p : α → Prop} → {r : Prop} → (∀ x, r → p x) ↔ (r → ∀ x, p x) :=
+def fp_style :
+  {α : Sort u} → {p : α → Prop} → {r : Prop} →
+  (∀ x, r → p x) ↔ (r → ∀ x, p x)
+:=
   Iff.intro
     (fun haxirpx hr x => (haxirpx x) hr)
     (fun hirap x hr => (hirap hr) x)
@@ -199,7 +272,7 @@ end ex18
 namespace ex19
 
 def even : Nat → Prop :=
-  sorry
+  fun n => ∃n' : Nat, n = 2 * n'
 
 end ex19
 
